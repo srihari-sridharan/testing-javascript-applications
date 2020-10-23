f8e7edb2d059ec38abd0ceebed484d7d1186f48a
Srihari Sridharan <srihari.sridharan.r@gmail.com>
2020-10-24 00:19:48 +0530

    Code changes

--- chapter4/2_testing_http_endpoints/1_using_supertest/logger.js
+++ chapter4/2_testing_http_endpoints/1_using_supertest/logger.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
+const path = require("path");
 
 const logger = {
-  log: msg => fs.appendFileSync("/tmp/logs.out", msg + "\n")
+  log: msg => fs.appendFileSync("tmp\\logs.out", msg + "\n")
 };
 
 module.exports = logger;
